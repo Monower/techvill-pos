eca00a514f3d3de240f9883837f8b1a73536caff
monower <sadafahmed720@gmail.com>
2024-02-25 00:26:35 +0600

    worked on the navbar

---
+++ app/_components/Navbar.jsx
@@ -0,0 +1,51 @@
+const Navbar = () => {
+    return (
+        <section className="container mx-auto px-2 lg:px-4 py-2 flex justify-between items-center">
+            <div className="">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer" className="cursor-pointer">
+                        <svg className="w-6 h-7  fill-indigo-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+                    </label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <button className="flex items-center space-x-2 bg-indigo-100 py-2 px-3 rounded font-medium group hover:bg-indigo-600 shadow-md">
+                <svg className="w-5 h-5 fill-indigo-600 group-hover:fill-indigo-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" /></svg>
+                <span className="text-indigo-600 text-[14px] group-hover:text-indigo-100">
+                    Note
+                </span>
+            </button>
+            <button className="flex items-center space-x-2 bg-indigo-100 py-2 px-3 rounded font-medium group hover:bg-indigo-600 shadow-md">
+                <svg className="w-5 h-5 fill-indigo-600 group-hover:fill-indigo-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" /></svg>
+                <span className="text-indigo-600 text-[14px] group-hover:text-indigo-100">
+                    Shipping
+                </span>
+            </button>
+            <button className="flex items-center space-x-2 bg-indigo-100 py-2 px-3 rounded font-medium group hover:bg-indigo-600 shadow-md">
+                <svg className="w-5 h-5 fill-indigo-600 group-hover:fill-indigo-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" /></svg>
+                <span className="text-indigo-600 text-[14px] group-hover:text-indigo-100">
+                    Hold Orders
+                </span>
+            </button>
+            <button className="flex items-center space-x-2 bg-indigo-100 py-2 px-3 rounded font-medium group hover:bg-indigo-600 shadow-md">
+                <svg className="w-5 h-5 fill-indigo-600 group-hover:fill-indigo-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" /></svg>
+                <span className="text-indigo-600 text-[14px] group-hover:text-indigo-100">
+                    New Item
+                </span>
+            </button>
+        </section>
+    )
+};
+
+export default Navbar;
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./_components/Navbar";
+
+const Home = () => {
+  return (<>
+    <Navbar />
+  </>)
+};
+
+export default Home;
